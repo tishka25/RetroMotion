c978572b1404e58a61843ef8930e894f8b798a66
root <alexander.petrov420@gmail.com>
2018-10-19 15:47:28 +0300

    test

--- NodeDeploy.sh
+++ NodeDeploy.sh
@@ -57,7 +57,7 @@ else
 fi;
 
 
-docker run -d --expose=3000 --expose=4000 -p $p:3000 -p $s:4000 -v $d:/Node $u $n noxid/ducks:v1
+docker run -d --expose=3000 --expose=4000 -p $p:3000 -p $s:4000 -v $d:/Node $u $n noxid/ducks:v2
 sleep 1
 docker exec -d -it $add bash ./Test.sh
 sleep 5
