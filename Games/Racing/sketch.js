63481fbdeebdbdf458842f53c9adbe8c6f42b490
root <alexander.petrov420@gmail.com>
2018-11-12 22:57:21 +0200

    cars

--- Games/Racing/sketch.js
+++ Games/Racing/sketch.js
@@ -1,40 +1,31 @@
+var img;
+var vid;
+var theta = 0;
+
 function setup(){
-    createCanvas(window.innerWidth , window.innerHeight ,WEBGL);
-    angleMode(DEGREES);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  img = loadImage("assets/t.png");
+ 
 }
 
-
-
-var x = -275 , y = 175 , z = 0;
 function draw(){
-    background(100);
-	
-	// noStroke();
-	fill(50);
-	push();
-	translate(x, y , z);
-	rotateY(0);
-    rotateX(90);
-    rotateZ(0);
+  background(0);
+  
+  push();
+  translate(440,0,0);
 
-    // box(100);
-    plane(width,height);
-	pop();
-
-	noFill();
-	stroke(255);
-	push();
-	translate(500, height*0.35, -200);
-	sphere(300);
-    pop();
     
-
-    if(keyIsPressed){
-        if(key==='w'){
-            y-=10;
-        }else if(key==='s'){
-            y+=10;
-        }
-        if()
-    }
+    rotateX(theta);
+    texture(img);
+    plane(1000, 1000);
+  pop();
+  if(keyIsPressed && key==='w'){
+    theta += 10;
+    console.log(theta);
+  }
+  if(keyIsPressed && key==='s'){
+    theta -= 10;
+    console.log(theta);
+  }
 }
