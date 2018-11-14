069112cc9772dcf23a823a90b7e2e896040c280d
root <alexander.petrov420@gmail.com>
2018-11-14 14:39:10 +0200

    picata

--- Games/Racing/sketch.js
+++ Games/Racing/sketch.js
@@ -1,7 +1,10 @@
 var img;
 var vid;
 var theta = 0;
-
+var gas=1;
+var x=600;
+var y=150;
+var z=460;
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
@@ -13,19 +16,31 @@ function draw(){
   background(0);
   
   push();
-  translate(440,0,0);
+  translate(x,y,z);
 
     
-    rotateX(theta);
+    rotateX(110);
+    rotateZ(theta);
+
     texture(img);
-    plane(1000, 1000);
+    plane(1600, 1600);
   pop();
   if(keyIsPressed && key==='w'){
-    theta += 10;
-    console.log(theta);
+    x*=1.01;
+    //y/=1.1;
+
   }
   if(keyIsPressed && key==='s'){
-    theta -= 10;
+    x/=1.01;
+    //y*=1.1;
+
+  }
+  if(keyIsPressed && key==='d'){
+    theta += 2;
+    console.log(theta);
+  }
+  if(keyIsPressed && key==='a'){
+    theta -= 2;
     console.log(theta);
   }
 }
