32052ec814b22227178807da225aa74384b4c261
Tedko <teodorstanishev@gmail.com>
2018-11-26 19:29:58 +0200

    PUSHVAM

--- Menu/GUI.js
+++ Menu/GUI.js
@@ -1,19 +1,42 @@
 class GUI {
   constructor(games) {
     this.cursor = cursor;
-    // this.game = game;
+    this.games = games;
     this.image;
     this.qrcode = createDiv('');
     this.hrefToConnect;
+    this.x = 200 ;
+    this.y = height * 0.5;
+    this.size = 400 * _pixelDensity;
   }
 
   begin() {
+    noStroke();
   }
 
   update(){
     push();
-    fill(100,0,0);
-    rect(20 * _pixelDensity, height*0.5 - 100, 200 * _pixelDensity, 200 * _pixelDensity );
+    imageMode(CENTER);
+    for(var i=0;i<this.games.length;i++){
+      var g = this.games[i];
+      var step = (this.size*2) * _pixelDensity * i;
+      var position = createVector(this.x + step , this.y);
+      this.mouseOver(position.x , position.y);
+      image(g.image , position.x ,position.y, this.size , this.size);
+    }
     pop();
+  }
+
+
+  mouseOver(x , y){
+    var d = dist(x,y , mouseX , mouseY);
+    var r = sqrt(2 * this.size * this.size) * 0.5;
+    if(d <= r){
+      push();
+      rectMode(CENTER);
+      fill(255 , 255 , 255 , 128);
+      ellipse(x , y , this.size * 1.2 , this.size * 1.2);
+      pop();
+    }
   }
 }
--- Menu/sketch.js
+++ Menu/sketch.js
@@ -1,15 +1,32 @@
-var gui;
+var gui = null;
 var _pixelDensity;
+var backgroundImage = null; 
+
+
+var DUCK_HUNT_URL;
+var FRUIT_NINJA_URL;
+
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
     _pixelDensity = height/1080;
-    gui = new GUI();
+    backgroundImage = loadImage("./assets/background_image.png");
+    gui = new GUI([
+        {
+            image:loadImage("./assets/duck_hunt_logo.jpg"),
+            name: "Duck Hunt"
+        },
+        {
+            image:loadImage("./assets/fruit_ninja_logo.png"),
+            name: "Fruit Ninja Masacre"
+        }
+    ]);
     gui.begin();
 }
 
 function draw(){
-    background(0);
+    imageMode(CORNER);
+    background(backgroundImage);
 
     gui.update();
 }
