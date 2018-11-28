1b66f95a83d33f314b89d88575d28e7d80f10619
root <alexander.petrov420@gmail.com>
2018-11-28 21:37:38 +0200

    Final

--- ScoreBoard/sketch.js
+++ ScoreBoard/sketch.js
@@ -4,9 +4,17 @@ var imageh;
 var clouds;
 var xClouds;
 var yClouds;
+
+var GAME_NAMES = ['Duck Hunt','Outrun','Fruit Ninja'];
+var currentGame = GAME_NAMES[0];
+
+
+
+
 function setup() {
   xClouds=-30;
   yClouds=random(20,windowHeight);
+
   this.socket=io.connect(document.location.host);//document.location.href
   this.socket.on('scores', function (data){
     HighScores=data;
@@ -23,29 +31,53 @@ function setup() {
     shot:false
   };
   this.socket.emit('dataIn',data);
-  
+
+  var buff = 0;
+  setInterval(function(){
+    if(buff>=GAME_NAMES.length){ buff = 0;}
+    currentGame = GAME_NAMES[buff];
+    buff++;
+  } , 2000);
+
+
 }
 
 function draw() { 
   background(imageh);
+
+
   textSize(80);
   image(clouds,xClouds,yClouds,400,200);
   xClouds+=0.5;
   if (xClouds>=windowWidth+500){
     xClouds=-500;
     yClouds=random(20,windowHeight/5);
-
   }
-  for(var i=0;i<HighScores.length;i++){
-    if(HighScores[i] !== undefined){
-      fill(0+i*50, 200-i*40, 0);
-    textSize(80 - i*5);
-    text((i+1)+ ")"+ HighScores[i].name + ":" + HighScores[i].score,10,  100 + (i*100));
+
+  var a = HighScores.filter(scores => scores.gamename == currentGame);
+
+  for(var i=0;i<a.length;i++){
+    textSize(150);
+    text(currentGame , 10,100);
+    var scores = a;    
+    if(currentGame !== "Outrun"){
+      scores = scores.sort(function( a , b){
+        return (b.score -a.score);
+      });
+      //scores.score[i]=formatTime(scores[i].score);
     }
-    
+    fill(0+i*50, 200-i*40, 0);
+    textSize(80 - i*5);
+    if(currentGame !== "Outrun"){
+
+    text((i+1)+ ")" + scores[i].name + ":" + scores[i].score,10,  200 + (i*100));
+    }else{
+      text((i+1)+ ")" + scores[i].name + ":" + formatTime(scores[i].score),10,  200 + (i*100));
+
+    }
   }
   WakeUP();  
-  console.log(HighScores);
+  // console.log(HighScores);
   
   
   // text('1st:'+HighScores[0].name,windowWidth/3.4 , windowHeight/5);
@@ -64,4 +96,15 @@ function draw() {
 
 function WakeUP(){
   this.socket.emit('scores',HighScores);
+}
+
+
+function formatTime(dt) {
+  var minutes = Math.floor(dt/60);
+  var seconds = Math.floor(dt - (minutes * 60));
+  var tenths  = Math.floor(10 * (dt - Math.floor(dt)));
+  if (minutes > 0)
+    return minutes + "." + (seconds < 10 ? "0" : "") + seconds + "." + tenths +"min";
+  else
+    return seconds + "." + tenths+"s";
 }
--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var app = express()
 //Variable so that the controller knows which page you are on
 var pageName = null;
 //
-var server=app.listen(3000,listening);
+var server = app.listen(3000, listening);
 //Websockets array
-  //CONSTANTS
-  const POSITION_X = 0;
-  const POSITION_Y = 1;
-  const POSITION_Z = 2;
-  const SHOT = 3;
-  const USER_NAME = 4;
-  const IS_EXIT = 5;
-  //
-var curData=[0,0];
+//CONSTANTS
+const POSITION_X = 0;
+const POSITION_Y = 1;
+const POSITION_Z = 2;
+const SHOT = 3;
+const USER_NAME = 4;
+const IS_EXIT = 5;
+//
+var curData = [0, 0];
 //
 
 ///socket 
@@ -45,16 +45,16 @@ client.connect();
 
 //SCORE INSERT FROM FRONTEND
 app.get('/insert/:name/:score/:gameName', function (req, res) {
-  var data =req.params;
+  var data = req.params;
   console.log(data);
-  client.query("INSERT INTO scores VALUES ('"+data.name+"',"+data.score+",'"+data.gameName+"');", (err, res) => {
+  client.query("INSERT INTO scores VALUES ('" + data.name + "'," + data.score + ",'" + data.gameName + "');", (err, res) => {
     if (err) {
       console.log(err.stack)
     } else {
       // console.log(res);
     }
+  });
 });
-});
 
 
 
@@ -62,40 +62,46 @@ app.get('/insert/:name/:score/:gameName', function (req, res) {
 
 
 
-var socket = require ('socket.io');
+var socket = require('socket.io');
 
-var io=socket(server);
+var io = socket(server);
 
 io.sockets.on('connection', newConnection);
 
-function newConnection(socket){
-  console.log('new connection:'+socket.id);
-  socket.on('dataIn',msg);
-  function msg(data){
-    data.x=Number(curData[POSITION_X]);
-    data.y=Number(curData[POSITION_Y]);
-    data.z=Number(curData[POSITION_Z]);
-    data.shot=curData[SHOT];
-    data.user_name= curData[USER_NAME];
+function newConnection(socket) {
+  console.log('new connection:' + socket.id);
+  socket.on('dataIn', msg);
+  function msg(data) {
+    data.x = Number(curData[POSITION_X]);
+    data.y = Number(curData[POSITION_Y]);
+    data.z = Number(curData[POSITION_Z]);
+    data.shot = curData[SHOT];
+    data.user_name = curData[USER_NAME];
     data.isExit = curData[IS_EXIT];
     pageName = data.page;
-    io.sockets.emit('dataIn',data);
+    io.sockets.emit('dataIn', data);
   }
 
-  socket.on('scores',msg2);
-  function msg2(data){
-    //TODO
-    client.query("SELECT * FROM scores WHERE name= 'polly' ORDER BY score DESC FETCH NEXT 5 ROW ONLY;", (err, res) => {
-      if (err) {
-        console.log(err.stack)
-      } else {
-        data=res.rows;
-        io.sockets.emit('scores',data);
-      }
-    // socket.broadcast.emit('dataIn',data);
+  var tableName = "";
+
+  socket.on('requestTable', function (data) {
+    tableName = data.toString();
+    console.log(tableName);
   });
 
-}
+  socket.on('scores', msg);
+  function msg(data) {
+      // client.query("SELECT * FROM scores WHERE gamename= '" + tableName + "' ORDER BY score DESC FETCH NEXT 5 ROW ONLY;", (err, res) => {
+      client.query("SELECT * FROM scores ORDER BY score ;",(err, res)=>{  
+      if (err) {
+          console.log(err.stack)
+        } else {
+          data = res.rows;
+          io.sockets.emit('scores', data);
+          console.log(data);
+        }
+      });
+    }
 }
 
 
@@ -105,12 +111,12 @@ function newConnection(socket){
 
 // });
 
-app.use('/mainmenu' , express.static("Menu"));
-app.use('/games/duckhunt',express.static("Games/DuckHunt"));
-app.use('/score',express.static("ScoreBoard"));
-app.use('/controller' , express.static("WebController"));
-app.use('/games/fruitninja',express.static("Games/FruitNinja"));
-app.use('/games/racing' , express.static("Games/Racing"));
+app.use('/mainmenu', express.static("Menu"));
+app.use('/games/duckhunt', express.static("Games/DuckHunt"));
+app.use('/score', express.static("ScoreBoard"));
+app.use('/controller', express.static("WebController"));
+app.use('/games/fruitninja', express.static("Games/FruitNinja"));
+app.use('/games/racing', express.static("Games/Racing"));
 
 
 const WebSocket = require('ws');
@@ -119,12 +125,12 @@ const wss = new WebSocket.Server({ port: 4000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    curData=JSON.parse(message);
+    curData = JSON.parse(message);
     ws.send(pageName);
     console.log(message);
   });
 });
 
-function listening(){
+function listening() {
   console.log("Listening...");
 }
